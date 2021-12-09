571b420eefc4ff85603edaf463960a4e2cd34fbe
David Melamed <david.melamed@web.de>
2021-12-09 14:15:17 +0200

    update some note

--- lab-work-n2/django_auth/django_auth/settings.py
+++ lab-work-n2/django_auth/django_auth/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# what happens if login successful
+LOGIN_REDIRECT_URL = 'home'
+
+# what happens if logout successful
+LOGOUT_REDIRECT_URL = 'login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'django_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lab-work-n2/django_auth/users/views.py
+++ lab-work-n2/django_auth/users/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
  
 def home(request):
-    return render(request,"users/home.html")
+    return render(request,"home.html")
+
+
+class SignUo(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
