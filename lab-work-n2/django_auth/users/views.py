0db176ddf15d4d157eb41ae2450e740937d21b66
David Melamed <david.melamed@web.de>
2021-12-09 14:31:48 +0200

    Finished main part

--- lab-work-n2/django_auth/users/views.py
+++ lab-work-n2/django_auth/users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
  
@@ -6,7 +9,7 @@ def home(request):
     return render(request,"home.html")
 
 
-class SignUo(CreateView):
+class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
